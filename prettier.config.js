a44d47efd75d85a93e56c8c64af88299948b8b5e
Kilian Tyler <kilian@kil.dev>
2025-09-04 18:31:56 -0400

    refactor: Overhaul Prettier configuration with new plugins

--- prettier.config.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
