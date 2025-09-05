dfc564b4034dbc931b820cb5bdb87c2935f1c593
Kilian Tyler <kilian@kil.dev>
2025-09-04 21:17:25 -0400

    fix: misplaced theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,11 @@ const geist = Geist({
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`} suppressHydrationWarning>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        <body className="flex min-h-screen flex-col">{children}</body>
-      </ThemeProvider>
+      <body className="flex min-h-screen flex-col">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
