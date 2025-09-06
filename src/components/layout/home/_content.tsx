fe5043af8fcd065af46d108916bd1c57d10be9d7
Kilian Tyler <kilian@kil.dev>
2025-09-06 19:52:48 -0400

    feat(hero): Improve Hero section layout and semantics

--- src/components/layout/home/_content.tsx
+++ src/components/layout/home/_content.tsx
@@ -1,5 +1,15 @@
-import { Hero } from './hero'
+import { HeroContent } from './hero/hero-content'
+import { ProfileImage } from './hero/profile-image'
 
 export function HomeContent() {
-  return <Hero />
+  return (
+    <section className="flex min-h-[calc(100svh-14rem)] md:min-h-[calc(100svh-16rem)] items-center px-10 py-20 md:px-20 lg:px-40">
+      <div className="mx-auto w-full max-w-7xl text-center lg:text-left">
+        <div className="grid grid-cols-1 items-center gap-16 lg:grid-cols-2">
+          <ProfileImage />
+          <HeroContent />
+        </div>
+      </div>
+    </section>
+  )
 }
