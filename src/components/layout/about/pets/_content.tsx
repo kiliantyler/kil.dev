66697a1b27e98e94840be94399eeafa2cd9edb2d
Kilian Tyler <kilian@kil.dev>
2025-09-06 22:42:11 -0400

    fix: Improves responsiveness of pet cards layout

--- src/components/layout/about/pets/_content.tsx
+++ src/components/layout/about/pets/_content.tsx
@@ -4,9 +4,9 @@ import { PETS } from '@/lib/pets'
 
 export function PetsContent() {
   return (
-    <div className="mt-12">
-      <SectionLabel>These are my pets</SectionLabel>
-      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
+    <div className="mt-12 px-6">
+      <SectionLabel className="mb-4">These are my pets</SectionLabel>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
         {PETS.map(pet => (
           <PetCard key={pet.id} pet={pet} />
         ))}
