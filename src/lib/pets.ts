8f55a0893dfd33bc1e544c54b04bb7f16426a1f8
Kilian Tyler <kilian@kil.dev>
2025-09-06 22:34:45 -0400

    feat: add some more dogs

--- src/lib/pets.ts
+++ src/lib/pets.ts
@@ -1,4 +1,6 @@
 import luxPhoto from '@/images/pets/lux.jpg'
+import milimPhoto from '@/images/pets/milim.jpg'
+import taliPhoto from '@/images/pets/tali.jpg'
 import type { Pet } from '@/types'
 
 export const PETS: Pet[] = [
@@ -12,9 +14,29 @@ export const PETS: Pet[] = [
     image: luxPhoto,
     imageAlt: 'Lux the Golden Retriever',
   },
+  {
+    id: 'milim',
+    name: 'Milim',
+    breed: 'Golden Retriever',
+    birthday: '2024-03-09',
+    gender: 'Female',
+    description:
+      "Milim is a crazy girl who can't sit still. She runs around like a maniac and loves to play with her sisters.",
+    image: milimPhoto,
+    imageAlt: 'Milim the Golden Retriever',
+  },
+  {
+    id: 'tali',
+    name: 'Tali',
+    breed: 'Mixed (Basenji/Australian Shepherd)',
+    birthday: '2015-09-24',
+    gender: 'Female',
+    description:
+      "Tali has a spinal cord injury but she doesn't let it slow her down. She loves to chase her sisters around the back yard and bark when they go too fast for her. She also loves to sleep on the couch.",
+    image: taliPhoto,
+    imageAlt: 'Tali the Mixed Breed (Basenji/Australian Shepherd)',
+  },
 ]
 
-//tali birthday 2015-9-24
 // Lilith & Azazel birthday 2021-04-13
 // Gozer birthday 2016-04-15
-// Milim birthday 2024-03-09
