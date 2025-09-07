1062c0ce2dc937c0194486df76acf93dc8bc6d16
Kilian Tyler <kilian@kil.dev>
2025-09-06 23:16:11 -0400

    feat(cats): Adds Gozer, Lilith, and Azazel to pets

--- src/lib/pets.ts
+++ src/lib/pets.ts
@@ -1,3 +1,6 @@
+import azazelPhoto from '@/images/pets/azazel.jpg'
+import gozerPhoto from '@/images/pets/gozer.jpg'
+import lilithPhoto from '@/images/pets/lilith.jpg'
 import luxPhoto from '@/images/pets/lux.jpg'
 import milimPhoto from '@/images/pets/milim.jpg'
 import taliPhoto from '@/images/pets/tali.jpg'
@@ -36,7 +39,36 @@ export const PETS: Pet[] = [
     image: taliPhoto,
     imageAlt: 'Tali the Mixed Breed (Basenji/Australian Shepherd)',
   },
+  {
+    id: 'gozer',
+    name: 'Gozer',
+    breed: 'Gozarian',
+    birthday: '2016-04-15',
+    gender: 'Female',
+    description: "When she isn't fighting the Ghostbusters, she loves to lay on laps and yell at anyone who walks by.",
+    image: gozerPhoto,
+    imageAlt: 'Gozer the Gozarian',
+  },
+  {
+    id: 'lilith',
+    name: 'Lilith',
+    breed: 'Witch Familiar',
+    birthday: '2021-04-13',
+    gender: 'Female',
+    description:
+      "There is nothing that Lilith wants more than to lay on my wife's lap and touch her face. When she can't do that she is laying with her brother.",
+    image: lilithPhoto,
+    imageAlt: 'Lilith the Witch Familiar',
+  },
+  {
+    id: 'azazel',
+    name: 'Azazel',
+    breed: 'Chonk',
+    birthday: '2021-04-13',
+    gender: 'Male',
+    description:
+      'Azazel is a very regal man who loves to sleep on the back of the couch. He regularly gets cleaned by his sister and hates it.',
+    image: azazelPhoto,
+    imageAlt: 'Azazel the Chonk',
+  },
 ]
-
-// Lilith & Azazel birthday 2021-04-13
-// Gozer birthday 2016-04-15
