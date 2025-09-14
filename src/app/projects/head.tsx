40c4b4388501626bd5d3f9fa454a244fc68753f2
Kilian Tyler <kilian@kil.dev>
2025-09-14 01:22:09 -0400

    fix: Limits preloaded images to the first 9

--- src/app/projects/head.tsx
+++ src/app/projects/head.tsx
@@ -3,7 +3,7 @@ import { projects } from '@/lib/projects'
 export default function Head() {
   return (
     <>
-      {projects.map(project => (
+      {projects.slice(0, 9).map(project => (
         <link key={project.id} rel="preload" as="image" href={project.imageSrc.src} />
       ))}
     </>
