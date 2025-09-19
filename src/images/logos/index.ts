cf489b2cb751da1dde158db9766487cad6aa8fb8
Kilian Tyler <kilian@kil.dev>
2025-09-19 13:18:54 -0400

    feat: add GE experience

--- src/images/logos/index.ts
+++ src/images/logos/index.ts
@@ -1,4 +1,5 @@
 export { default as DraftKingsLogo } from './draftkings.webp'
+export { default as GELogo } from './ge.webp'
 export { default as KaseyaLogo } from './kaseya.webp'
 export { default as NewInnovationsLogo } from './newinnov.webp'
 export { default as QGendaLogo } from './qgenda.webp'
