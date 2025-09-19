6c8fd82106bb0087d17b026febc65128bc872493
Kilian Tyler <kilian@kil.dev>
2025-09-19 17:31:59 -0400

    fix: Updates location information

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -3,8 +3,8 @@ import type { HomeContent } from '@/types/home-content'
 export const HOME_CONTENT: HomeContent = {
   NAME: 'Kilian Tyler',
   TITLE: 'Site Reliability | DevOps Engineer',
-  LOCATION: 'Mentor, Ohio',
+  LOCATION: 'Cleveland, Ohio',
   MAP_LATITUDE: 41.6662,
   MAP_LONGITUDE: -81.3395,
-  LOCATION_TOOLTIP: 'Just outside of Cleveland',
+  LOCATION_TOOLTIP: 'Just outside of Cleveland, actually.',
 }
