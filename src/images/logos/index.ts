2d070a4ecf97a1f2536f1fc595ce86b9e15196a6
Kilian Tyler <kilian@kil.dev>
2025-09-19 13:03:21 -0400

    feat: add kaseya logo

--- src/images/logos/index.ts
+++ src/images/logos/index.ts
@@ -1,3 +1,4 @@
 export { default as DraftKingsLogo } from './draftkings.webp'
+export { default as KaseyaLogo } from './kaseya.webp'
 export { default as NewInnovationsLogo } from './newinnov.webp'
 export { default as QGendaLogo } from './qgenda.webp'
