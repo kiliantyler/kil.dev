10a61640467e91d27ea75390d0d87ff1e916f1ea
Kilian Tyler <kilian@kil.dev>
2025-09-22 11:43:30 -0400

    feat(providers): Integrate ReviewProvider into main Providers composition

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -5,6 +5,7 @@ import type { UnlockedMap } from '@/utils/achievements'
 import { AchievementsProvider } from './achievements-provider'
 import { ConfettiProvider } from './confetti-provider'
 import { PostHogProvider } from './posthog-provider'
+import { ReviewProvider } from './review-provider'
 import { SnowProvider } from './snow-provider'
 import { ThemeProvider } from './theme-provider'
 
@@ -22,7 +23,9 @@ export function Providers({
       <ThemeProvider initialAppliedTheme={initialAppliedTheme}>
         <ConfettiProvider>
           <SnowProvider>
-            <AchievementsProvider initialUnlocked={initialUnlocked}>{children}</AchievementsProvider>
+            <AchievementsProvider initialUnlocked={initialUnlocked}>
+              <ReviewProvider>{children}</ReviewProvider>
+            </AchievementsProvider>
           </SnowProvider>
         </ConfettiProvider>
       </ThemeProvider>
